5908acc593a963e1623c5fbd96d0603083732448
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-08-03 12:51:00 +0300

    Add bucketlistitems models, serializer, view, url

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from api.views import BucketlistViewSet
+from api.views import BucketlistViewSet, BucketlistItemViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'bucketlists', BucketlistViewSet)
+router.register(r'items', BucketlistItemViewSet)
 
 urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,10 +1,21 @@
 # Create your views here.
 from rest_framework import viewsets
 
-from api.models import Bucketlist
-from api.serializers import BucketlistSerializer
+from api.models import Bucketlist, BucketlistItem
+from api.serializers import BucketlistSerializer, BucketlistItemSerializer
 
 
 class BucketlistViewSet(viewsets.ModelViewSet):
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class BucketlistItemViewSet(viewsets.ModelViewSet):
+    queryset = BucketlistItem.objects.all()
+    serializer_class = BucketlistItemSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
